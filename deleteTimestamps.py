77323ec1966de4785938a05bfbeb2785ac2617b4
Tommi0x <tommaso.toloni@icloud.com>
2023-10-03 10:20:15 +0200

    V1

---
+++ deleteTimestamps.py
@@ -0,0 +1,25 @@
+import re
+
+def remove_timestamps(text):
+    # Regular expression pattern to match timestamps in the format hh:mm
+    pattern = r'\b\d{1,2}:\d{2}\b'
+    # Use re.sub() to replace matched timestamps with an empty string
+    result = re.sub(pattern, '', text)
+    return result
+
+def remove_empty_lines(text):
+    # Remove empty lines using regex
+    cleaned_text = re.sub(r'\n\s*\n', '\n', text)
+    return cleaned_text.strip()  # Strip leading/trailing whitespace
+
+finput = open("file.txt", "r")
+foutput = open("output.txt", "a")
+
+text = remove_timestamps(finput.read())
+text = remove_empty_lines(text)
+print(text)
+
+foutput.write(text)
+foutput.close()
+
+print("OPERAZIONE COMPLETATA")
